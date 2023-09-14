d70803ef15e1809ac8987c28bea2742d6e61619e
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-14 17:06:32 +0800

    search bar and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [city, setCity] = useState("");
+  
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <div className='input-container'>
+        <input 
+          className='inputs'
+          type='text' 
+          placeholder='Enter a City' 
+          onChange={(e) => setCity(e.target.value)}
+          />
+        <button>Get Weather</button>
+      </div>
     </div>
   );
 }
