dd1b2c9f46243d55fa91cda2fe1ddbc214d015e3
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-15 21:46:00 +0800

    fetch data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import axios from 'axios';
 
 function App() {
   const [city, setCity] = useState("");
-  const [isloading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  const [showData, setShowData] = useState(false);
+  const [currentDay, setCurrentDay] = useState(0);
 
   const handleClick = () => {
     setIsLoading(true);
     axios.get(`https://goweather.herokuapp.com/weather/${city}`)
           .then((response) => {
+            setIsLoading(false);
+            setWeatherData(response.data);
+            setShowData(true);
             console.log(response)
           })
   }
@@ -24,6 +30,31 @@ function App() {
           onChange={(e) => setCity(e.target.value)}
           />
         <button onClick={handleClick}>Get Weather</button>
+      </div>
+      <div>
+        {isLoading && <h1>Data Loading...</h1>}
+        {showData && (
+          <div>
+            <h1>{city}</h1>
+            <h3>{weatherData.description}</h3>
+            <h1>{weatherData.temperature}</h1>
+            <div>
+              <div>
+                <button> &larr; </button>
+              </div>
+              <div>
+                <div>
+                  <h1>Day {weatherData.forecast[0].day}</h1>
+                  <h1>{weatherData.forecast[0].temperature}</h1>
+                  <h1>{weatherData.forecast[0].wind}</h1>
+                </div>
+              </div>
+              <div>
+                <button> &rarr; </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
